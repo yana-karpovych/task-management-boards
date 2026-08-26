8703c03b334ea0b4a120e2124c730abf741928db
Yana Karpovych <yanakarpovych0707@gmail.com>
2026-08-26 19:17:13 +0300

    feat: add card prisma model

---
+++ backend/prisma/migrations/20260826161450_add_card/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "Column" AS ENUM ('TODO', 'IN_PROGRESS', 'DONE');
+
+-- CreateTable
+CREATE TABLE "Card" (
+    "id" TEXT NOT NULL,
+    "boardId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "column" "Column" NOT NULL,
+    "position" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Card_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Card_boardId_column_position_idx" ON "Card"("boardId", "column", "position");
